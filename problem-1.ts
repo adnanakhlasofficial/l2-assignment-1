6d2972cef00b18319cce5f1bfbd41a73ea418f4f
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-05-27 14:59:52 +0600

    solved: problem 1

---
+++ problem-1.ts
@@ -0,0 +1,9 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || typeof toUpper === "undefined") {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+
+console.log(formatString("Hello", false));
